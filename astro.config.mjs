1ae45fbad822d1040a0477e8920a2fc105732556
maltenz <hello@malte.nz>
2025-07-27 09:50:47 +1200

    bugfix: localhost and server resolve descrepancy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,29 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
-// https://astro.build/config
+function rewriteConfigPlugin() {
+  return {
+    name: 'rewrite-config-plugin',
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        /* 
+        * bugfix: localhost and server resolve descrepancy
+        */
+        if (req.url === '/config.yml') {
+          req.url = '/admin/config.yml';
+        }
+        next();
+      });
+    },
+  };
+}
+
 export default defineConfig({
   integrations: [react()],
+  vite: {
+    plugins: [rewriteConfigPlugin()],
+  },
   server: {
-    port: 3000
-  }
+    port: 3000,
+  },
 });
