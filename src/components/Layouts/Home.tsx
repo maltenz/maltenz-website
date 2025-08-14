0147e7a6a0752353372f5eb99de9ba8058497ae7
maltenz <hello@malte.nz>
2025-08-15 11:46:15 +1200

    Delete emblems

--- src/components/Layouts/Home.tsx
+++ src/components/Layouts/Home.tsx
@@ -3,7 +3,6 @@ import type { HeroData, ProjectEntry } from '../../types/content';
 import AppWrapper from '../AppWrapper';
 import Hero from '../Hero';
 import Latest from '../Projects/Latest';
-import Emblems from './lib/Emblems';
 
 type HomeProps = {
   heroData: HeroData;
@@ -15,7 +14,6 @@ export default function Home({ heroData, projectsData }: HomeProps) {
     <AppWrapper>
       <Hero data={heroData} variant="full" />
       <Latest data={projectsData} />
-      <Emblems />
       <Footer />
     </AppWrapper>
   );
